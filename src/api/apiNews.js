3ef224c35e2258b5f319d8540898e7eb15f63a8a
elisha111 <elisey.kovalyov@yandex.ru>
2025-10-02 21:52:01 +0300

    #4 feat(category): компонент категорий + новый запрос на категории

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,14 +3,32 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async (page=1, pageSize=10) => {
+export const getNews = async (props) => {
+  const { page = 1, pageSize = 10, category } = props;
+
   try {
-    const response = await axios.get(`${BASE_URL}everything`, {
+    const response = await axios.get(`${BASE_URL}top-headlines`, {
       params: {
         apiKey: API_KEY,
-        q: "apple",
+        language: "en",
         page,
         pageSize,
+        category,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}top-headlines/sources`, {
+      params: {
+        apiKey: API_KEY,
+        language: "en",
       },
     });
 
