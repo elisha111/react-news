10e881c85935044205a35bebb856380658c95388
elisha111 <elisey.kovalyov@yandex.ru>
2025-10-03 20:37:35 +0300

    #6 refactor: рефакторинг страницы main

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,6 +1,7 @@
 import { formatTimeAgo } from "@/helpers/formatTimeAgo";
 import Image from "@/components/Image";
 import styles from "./styles.module.css";
+import withSkeleton from "@/helpers/hocs/withSkeleton";
 
 const NewsBanner = (props) => {
   const { item } = props;
@@ -8,12 +9,14 @@ const NewsBanner = (props) => {
   return (
     <div className={styles.root}>
       <Image image={item?.image} />
-      <h3 className={styles.title}>{item.title}</h3>
+      <h3 className={styles.title}>{item?.title}</h3>
       <p className={styles.extra}>
-        {formatTimeAgo(item.published)} by {item.author}
+        {formatTimeAgo(item?.published)} by {item?.author}
       </p>
     </div>
   );
 };
 
-export default NewsBanner;
+const NewsBannerWithSkeleton = withSkeleton(NewsBanner, "banner", 1);
+
+export default NewsBannerWithSkeleton;
